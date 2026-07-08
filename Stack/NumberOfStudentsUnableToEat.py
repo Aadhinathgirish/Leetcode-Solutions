af416a2fd2b637a31dbd1a9a5f1f814b2718b1df
Aadhi <aadhinathgirish@gmail.com>
2026-07-08 11:28:52 +0530

    Number of Students Unable to Eat

---
+++ Stack/NumberOfStudentsUnableToEat.py
@@ -0,0 +1,7 @@
+def eat(students,sandwiches):
+    count = [students.count(0),students.count(1)]
+    for i in sandwiches:
+        if count[i] == 0:
+            break
+        count[i]-=1
+    return sum(count)
