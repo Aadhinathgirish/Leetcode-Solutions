eb0155496051fdcff346afcdba542024f36a5899
Aadhi <aadhinathgirish@gmail.com>
2026-08-02 12:07:42 +0530

    Final Price With Discount Price

---
+++ Stack/FinalPriceWithDiscount.py
@@ -0,0 +1,13 @@
+class Solution:
+    def finalPrices(self, prices):
+        ans = [0] * (len(prices))
+        stack = []
+        for i in range(len(prices)-1,-1,-1):
+            while stack and stack[-1] > prices[i]:
+                stack.pop()
+            if stack:
+                ans[i] = prices[i] - stack[-1]
+            else:
+                ans[i] = prices[i]
+            stack.append(prices[i])
+        return ans
