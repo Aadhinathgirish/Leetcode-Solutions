46e3ca1c3949ef20ced77738b87d059c53327b00
Aadhi <aadhinathgirish@gmail.com>
2026-08-07 10:58:35 +0530

    Two Sum Array Sorted

---
+++ Two pointers/twosumarraysorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, number,target: int):
+        l = 0
+        r = len(number) - 1
+        while l < r:
+            if number[l] + number[r] == target:
+                return [l+1,r+1]
+            elif number[l] + number[r] > target:
+                r-=1
+            else:
+                l+=1
+            
